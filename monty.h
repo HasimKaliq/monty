23691516b335b0d85916b86dd63f70fadff0cda5
hasim kaliq <hasimkaliq@gmail.com>
2022-12-16 12:12:07 -0800

    Added guards to the header file

--- monty.h
+++ monty.h
@@ -1,3 +1,5 @@
+#ifndef MONTY_H
+#define MONTY_H
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -27,3 +29,4 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+#endif
